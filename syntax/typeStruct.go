913c53ad23f4d0b35d94f6feaf2e2e2c569c79b8
yuzi <hayuzi@163.com>
2018-11-30 16:50:11 +0800

    struct

--- syntax/typeStruct.go
+++ syntax/typeStruct.go
@@ -27,6 +27,9 @@ type tree struct {
 	left, right *tree
 }
 
+type Point struct{ X, Y int}
+
+
 func main() {
 	var dilbert Employee
 
@@ -50,6 +53,48 @@ func main() {
 	fmt.Println(s1)
 	Sort(s1)
 	fmt.Println(s1)
+	fmt.Println(employeeOfTheMonth)
+
+
+	// =====================
+	// 5. 结构体字面量
+	// 格式一： 该格式必须按照正确的顺序给全部数据赋值， 一般用于有明显成员变量顺序约定的小结构体重
+	p1 := Point{1, 2}
+
+	// 格式二： 指定附文后者全部成员变量的名称和值来初始化结构体变量， 可以不考虑顺序
+	// 这种方式如果有成员变量没有指定，那么该成员变量的值会是其类型的 零值
+	p2 := Point{X: 1, Y: 2}
+
+	// 以上两种初始化方式不可以混合使用，也不可以用第一种初始化方式来绕过不可导出变量无法在其他包中使用的规则
+	fmt.Println(p1, p2)
+
+
+	// ============
+	// 6. 结构体类型的值可以作为参数传递给函数或者作为函数的返回值
+	fmt.Println(Scale(Point{1,2}, 5))
+
+	// 出于效率的考虑，大型结构体通常使用结构体指针的方式直接传递给函数或者从函数中返回
+	// 这种方式在函数需要修改结构体内容的时候也是必须的，
+	// ** 在Go这种按照值调用的语言中，调用的函数接收到的是实参的一个副本，并不是实参的引用
+ 	employeeOfTheMonth.Salary = 10000
+	fmt.Println(dilbert)
+	Bonus(employeeOfTheMonth, 200)
+	fmt.Println(dilbert)
+
+	// 7. 由于通常结构体都通过指针的方式使用， 因此可以使用一种简单的方式来创建/初始化一个struct类型的变量并获取他的地址
+	pp := &Point{1,2}
+	fmt.Println(pp)
+
+	pp2 := new(Point)
+	*pp2 = Point{1,2}
+	fmt.Println(pp2)
+
+
+
+
+
+
+
 
 }
 
@@ -84,4 +129,17 @@ func add(t *tree, value int) *tree {
 		t.right = add(t.right, value)
 	}
 	return t
+}
+
+func Scale(p Point, factor int) Point {
+	return Point{ p.X * factor, p.Y * factor}
+}
+
+
+func Bonus(e *Employee, percent int) int {
+	return e.Salary * percent / 100
+}
+
+func AwardAnnualRaise(e * Employee) {
+	e.Salary = e.Salary * 105 /100
 }
