249c90784d6c2fee2dff7a10bf0c4de79db756f1
yuzi <hayuzi@163.com>
2018-12-11 10:59:37 +0800

    interface

--- syntax02/interface.go
+++ syntax02/interface.go
@@ -1,1 +1,99 @@
 package main
+
+import "fmt"
+
+// ===================
+// 1. 接口即约定，该处暂不做详细解释
+
+
+// ===================
+// 2. 接口类型
+// 一个接口类型定义了一套方法，如果一个具体类型要实现该接口，那么必须实现接口类型定义中的所有方法
+type Reader interface {
+	Read(p []byte) (n int, err error)
+}
+
+type Writer interface {
+	Write(p []byte) (n int, err error)
+}
+
+// 通过组合已有接口可以得到新接口，这样的语法称为嵌入式接口
+// 但是也可以不用嵌入式来声明
+type ReadWriter interface {
+	Reader
+	Writer
+}
+
+
+// 以下是一个示例
+type Human struct {
+	name string
+	age int
+	phone string
+}
+
+type Student struct {
+	Human //匿名字段Human
+	school string
+	loan float32
+}
+
+type Employee struct {
+	Human //匿名字段Human
+	company string
+	money float32
+}
+
+//Human对象实现Sayhi方法
+func (h *Human) SayHi() {
+	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
+}
+
+// Human对象实现Sing方法
+func (h *Human) Sing(lyrics string) {
+	fmt.Println("La la, la la la, la la la la la...", lyrics)
+}
+
+//Human对象实现Guzzle方法
+func (h *Human) Guzzle(beerStein string) {
+	fmt.Println("Guzzle Guzzle Guzzle...", beerStein)
+}
+
+// Employee重载Human的Sayhi方法
+func (e *Employee) SayHi() {
+	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name,
+		e.company, e.phone) //此句可以分成多行
+}
+
+//Student实现BorrowMoney方法
+func (s *Student) BorrowMoney(amount float32) {
+	s.loan += amount // (again and again and...)
+}
+
+//Employee实现SpendSalary方法
+func (e *Employee) SpendSalary(amount float32) {
+	e.money -= amount // More vodka please!!! Get me through the day!
+}
+
+// 定义interface
+type Men interface {
+	SayHi()
+	Sing(lyrics string)
+	Guzzle(beerStein string)
+}
+
+type YoungChap interface {
+	SayHi()
+	Sing(song string)
+	BorrowMoney(amount float32)
+}
+
+type ElderlyGent interface {
+	SayHi()
+	Sing(song string)
+	SpendSalary(amount float32)
+}
+
+func main() {
+
+}
