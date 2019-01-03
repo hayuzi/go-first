aaa4d219398eaf9ff756f687909320e58ebbc20f
yuzi <hayuzi@163.com>
2019-01-03 14:51:34 +0800

    interface

--- syntax02/reflection.go
+++ syntax02/reflection.go
@@ -3,6 +3,8 @@ package main
 import (
 	"reflect"
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
@@ -20,7 +22,27 @@ func main() {
 	fmt.Println(t.String()) // "int"
 	fmt.Println(t)          // "int"
 
+	// 因为 reflect.TypeOf 返回一个接口值对应的动态类型，所以它返回的总是具体类型（ 而不是接口类型 ）
 
+	var w io.Writer = os.Stdout
+	fmt.Println(reflect.TypeOf(w)) // 是 *os.Files 而不是 io.Writer
+
+	// reflect.Value 可以包含一个任意类型的值
+	// reflect.ValueOf函数接收任意的interface{} 并将接口的动态值以reflect.Value的形式返回
+	// 与reflect.TypeOf类似, reflect.ValueOf的返回值也都是具体值，不过 reflect.Value也可以包含一个接口值
+	v := reflect.ValueOf(3) // 一个reflect.Value
+	fmt.Println(v)          // "3"
+	fmt.Printf("%v\n", v)   // 3
+	fmt.Println(v.String()) // 注意： "<int Value>"
+	// reflect.Value也满足fmt.Stringer, 但除非Value包含的是一个字符串，否则 String方法的结果仅仅暴露了类，
+	// 通常需要使用 fmt包的%v 功能，它对 reflect.Value会进行特殊处理
+
+	// 调用 value的Type方法会把它的类型以 reflect.Type方式返回
+	t2 := v.Type()
+	fmt.Println(t2.String()) // "int"
+
+	// reflect.ValueOf的逆操作是 reflect.Value.Interface 方法。它返回一个 interface{} 接口值，
+	// 与reflect.Value包含同一个具体值
 
 
 
